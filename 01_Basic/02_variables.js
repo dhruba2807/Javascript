d3fb39987d1d10c482434458256c1eff98099358
Dhruba Mondal <126279399+dhruba2807@users.noreply.github.com>
2023-12-21 18:56:25 +0000

    config

--- 01_Basic/02_variables.js
+++ 01_Basic/02_variables.js
@@ -3,8 +3,15 @@ let  accountEmail = "sammydhruba@gmail.com"
 var accountPassword = "12345"
 
 
+
+accountEmail = "abc@gmail.com"
+accountPassword ="544654"
+accountCity = "Kolkata"
+
 accountCity = "Jaipur"
 
 // accountId = 2
-
+/*
+PREFER NOT TO USE VAR BECAUSE OF ISSUE IN BLOCK SCOPE AND FUNCTIONAL SCOPE
+*/
 console.table([accountEmail,accountId,accountPassword,accountCity]);
