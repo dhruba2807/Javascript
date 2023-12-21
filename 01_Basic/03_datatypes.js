552b92701c64aad91fe6e024162f691ecacb16ba
Dhruba Mondal <126279399+dhruba2807@users.noreply.github.com>
2023-12-21 19:08:39 +0000

    datatypes

---
+++ 01_Basic/03_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // Treat All JS as newer version
+
+
+// alert(3+3)  We Are Using Nodejs Not Browser
+
+
+// tc39.es and mdn This Are Website For Help Javascript
+
+
+let name = "Dhruba Mondal" //String
+let number = 12 //number => 2 to power 53
+let isLoggedin = true //BOOLEAn
+
+// bigint
+// null =>standalone value
+// undefined => 
+// Symbol= =>unique (Use In React)
+
+// Object
+
+console.log(typeof number);
