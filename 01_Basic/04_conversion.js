649c8e861f92c27330c6eb9456a880297de55221
Dhruba Mondal <126279399+dhruba2807@users.noreply.github.com>
2023-12-22 18:14:31 +0000

    Conversion of Data Types

---
+++ 01_Basic/04_conversion.js
@@ -0,0 +1,24 @@
+let score =  "33"   //  THIS IS NOW A STRING 
+console.log(typeof score)
+console.log(typeof(score))
+
+let a = "abc123"    // THIS IS STRING 
+let b = Number(a)       // BUT NOW THIS IS A NUMBER 
+console.log(a)
+console.log(typeof a)
+console.log(b)
+console.log(typeof b)     //  "NaN"  MEANS NOT Any Number      
+
+
+let Valueinnumber = Number(score)
+
+console.log(typeof(Valueinnumber))
+
+
+
+//   1 => true    0 => false
+
+
+    //  ""  =>false
+
+    // "Hitesh"  => True
