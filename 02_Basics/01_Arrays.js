582364cee3a98e751961ac757ba833f7ca14dd35
Dhruba Mondal <126279399+dhruba2807@users.noreply.github.com>
2023-12-24 17:57:05 +0000

    array First Class

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,50 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+
+// UNSHIFT MEANS VALUE INSERT AT FIRST POSITION
+// myArr.unshift(9)
+// SHIFT MEANS VALUE SHIFT(REMOVE) AT FIRST POSITION
+// myArr.shift()
+
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+
+//JOIN MEANS ARRAY CONVERT TO STRING
+// console.log(myArr);
+console.log(typeof(myArr));
+// console.log( newArr);
+console.log(typeof(newArr));
+
+
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
