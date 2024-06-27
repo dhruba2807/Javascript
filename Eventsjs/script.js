480db3dc33ce39447b97023c981e956d7666396c
Dhruba Mondal <126279399+dhruba2807@users.noreply.github.com>
2024-06-27 22:48:59 +0000

    events

---
+++ Eventsjs/script.js
@@ -0,0 +1,9 @@
+// document.getElementById('ram').onclick = function(){
+//     alert("jay Shree Ram")
+// }
+document.getElementById('ram').addEventListener('click', function () {
+    alert("Jay shree Ram")
+});
+document.getElementById('').addEventListener('click',function(){
+    alert("Jay shree Ram")
+})
